d04d642329d56ab808ce8e0017357b8d1e48f6aa
Britt Crawford <britt@illtemper.org>
2009-12-07 19:39:19 -0800

    Basic bundler based configration

---
+++ bundler.rb
@@ -0,0 +1,19 @@
+# Basic configuration using bundler
+run "rm public/index.html"
+
+git :init
+
+file 'Gemfile', <<-Gemfile
+source "http://gems.github.com"
+disable_system_gems
+
+gem "rails", "2.3.5"
+gem "haml", ">= 2.2.9"
+
+gem "rspec", :only => :testing
+gem "rspec-rails", :lib => 'spec/rails', :only => :testing
+Gemfile
+
+file 'config/preinitializer.rb', <<-Preinitializer
+  require "\#\{RAILS_ROOT\}/vendor/gems/environment"
+Preinitializer
